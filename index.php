a55400142ce7c17d77c70ddd2d69190b997f0a4b
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-05-03 03:20:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning php</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="" alt="Hair salon logo">
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Stylist</li>
+            <li>Gallery</li>
+            <li>About Us</li>
+            <li>Contact</li>
+        </ul>
+        <button>Book Appointment</button>
+    </nav>
+    
+    <div class="hero">
+        <h1>Hair salon</h1>
+        <p>Hair salon</p>
+        <button>Book Appointment</button>
+    </div>
+
+    
+
+    
+</body>
+</html>
+<?php
+   
+
+    
+    
+    
+?>
